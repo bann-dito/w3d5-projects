5c9f96026287e34e71f9d7aeb528e7b3deb4299d
bann-dito <ed.bacsierra@gmail.com>
2023-02-17 14:13:23 -0800

    initialize method and WIP parent method

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
+
+        attr_reader :parent, :children, :value
+
+        def initialize(value, parent = nil, children = [])
+            @value = value
+            @parent = parent
+            @children = children
+
+        end
+
+        def parent=(new_parent)
+
+            @parent = new_parent
+            if !@parent.children.include?(self)
+                new_parent.children << self
+            else
+                @children
+            end
+
+
+            
+        end
   
 end
