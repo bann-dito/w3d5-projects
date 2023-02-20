eb2c34430600daeabf31364a57927bc1fc39be94
bann-dito <ed.bacsierra@gmail.com>
2023-02-20 15:26:17 -0800

    check for bugs

--- practice-for-ch-ruby-knights-travails-long-practice-main/knights.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knights.rb
@@ -24,9 +24,42 @@ class KnightPathFinder
 
     end
 
-    def new_move_oositions(pos)
-        @considered_positions = []
-        @considered_positions << KnightPathFinder.valid_moves(pos)
+    def new_move_positions(pos)
+
+        moves = KnightPathFinder.valid_moves(pos)
+        moves.select! { |move| !@considered_positions.include?(move) }
+        @considered_positions += moves
+        moves
+        
+
+    end
+
+    def build_move_tree
+
+        queue = [@root_node]
+
+
+        until queue.empty?
+            #iterate through queue
+            #var = queue.shift 
+            #pass var to new_moveposition
+            #assign var to eval of new_moveposition
+            #iterate through var to make each polytree node class
+            #shovel to add nodes to queue
+            #add thos to our queue
+
+
+            prev_nodes = queue.shift
+            moves = new_move_positions(prev_nodes.value)
+            moves.each do |pos|
+                new_node = PolyTreeNode.new(pos, prev_nodes)
+                prev_nodes.add_child(new_node)
+                queue << new_node
+            end
+
+        end
+
+
         
     end
 
